a13e269f340f75c3950fd72ae59df9acdcb4fd5f
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2023-12-25 15:41:26 -0300

    Add longest common prefix function to the code

--- longestCommonPrefix.ts
+++ longestCommonPrefix.ts
@@ -1,3 +1,16 @@
 function longestCommonPrefix(strs: string[]): string {
-    
+  let sortedAlphabetically = strs.sort((a,b) => a < b? -1:1);
+
+  let firstWord = sortedAlphabetically[0];
+  let lastWord = sortedAlphabetically[sortedAlphabetically.length - 1];
+
+  let prefix = '';
+  // After sorting, if the first and last word have a common prefix, 
+  // then all the words in the array have that prefix
+  for (let i = 0; i < firstWord.length; i++) {
+    if (firstWord[i] === lastWord[i]) prefix += firstWord[i];
+    else break;
+  }
+
+  return prefix;
 };
