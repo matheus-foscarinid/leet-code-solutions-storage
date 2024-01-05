5a8521c7a5d88491e6e5555b82c0a65904c686a6
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2024-01-04 22:18:08 -0300

    Add reverseString function to solutions directory

---
+++ solutions/reverseString.ts
@@ -0,0 +1,12 @@
+function reverseString(s: string[]): void {
+  let p1 = 0;
+  let p2 = s.length - 1;
+
+  while (p1 < p2) {
+    const temp = s[p1]
+    s[p1] = s[p2];
+    s[p2] = temp;
+    p1++;
+    p2--;
+  }
+};
